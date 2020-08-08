8d90e5b4645379ac988be390032e20a557e38a15
Armer Song <armer_song@qq.com>
2020-08-08 22:46:16 +0800

    [Update]

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 export RUST_TARGET_PATH=`pwd`
+# build for custom std
 CC=arm-himix100-linux-gcc xargo build --target armv7-himix100-linux-gnueabihf --release
+# build for app
+#xargo rustc --target armv7-himix100-linux-gnueabihf --release
 arm-himix100-linux-strip target/armv7-himix100-linux-gnueabihf/release/hello
