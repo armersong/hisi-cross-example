e21c2bd1489c8e4e929738b6df183d3286d13223
Armer Song <armer_song@qq.com>
2020-08-09 09:02:55 +0800

    [BugFix] bindgen compile fail

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 export RUST_TARGET_PATH=`pwd`
-# build for custom std
-CC=arm-himix100-linux-gcc xargo build --target armv7-himix100-linux-gnueabihf --release
-# build for app
-#xargo rustc --target armv7-himix100-linux-gnueabihf --release
-arm-himix100-linux-strip target/armv7-himix100-linux-gnueabihf/release/hello
+HOST_CC=gcc CC=arm-himix100-linux-gcc xargo build --target armv7-unknown-linux-uclibc --release
+arm-himix100-linux-strip target/armv7-unknown-linux-uclibc/release/hello
